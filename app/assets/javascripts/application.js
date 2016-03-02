4fa284190be728eede9018ba0d7883e80e66168a
Diego R. Brogna <dierbro@gmail.com>
2016-03-02 09:15:52 +0100

    Add homebuyers animation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,6 +44,61 @@ $(document).ready(function(){
       href: 'https://www.strideup.com',
     }, function(response){});
   });
+
+
+  if($("#animated-bars").length){
+    $("#animated-bars .bars div").each(function(){
+      $(this).css('margin-top', $(this).parent().height()-$(this).height());
+    })
+    setInterval(function(){
+      var current = $("#animated-bars").data("currentbar");
+      $("#animated-bars .bars div").removeClass("active-bar");
+      var new_bar = current == 10 ? 1 : (current+1);
+      $("#animated-bars #bar"+new_bar).addClass("active-bar");
+      $("#animated-bars").data("currentbar", new_bar);
+    }, 500);
+  }
+
+  if($("#map").length){
+
+    //Render UK map
+    var width = parseInt(d3.select('#map .uk-map').style('width')),
+    map_ratio = 0.8
+    height = width * map_ratio;
+
+    var projection = d3.geo.albers()
+    .center([0, 55.4])
+    .rotate([4.4, 0])
+    .parallels([50, 60])
+    .scale(1200 * 5)
+    .translate([width / 2, height / 2]);
+
+    var path = d3.geo.path()
+    .projection(projection);
+
+    var svg = d3.select("#map .uk-map").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+    d3.json("/uk-postcode-area.json", function(error, uk) {
+      svg.selectAll(".subunit")
+      .data(topojson.feature(uk, uk.objects['uk-postcode-area']).features)
+      .enter().append("path")
+      .attr("class", "feature")
+      .attr("d", path)
+      .append("svg:title")
+      .attr("transform", function (d) { return "translate(" + path.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function (d) { return d.id; });
+
+      svg.append("path")
+      .datum(topojson.mesh(uk, uk.objects['uk-postcode-area'], function(a, b) { return a !== b; }))
+      .attr("class", "mesh")
+      .attr("d", path);
+
+    });
+  }
+
 });
 
 
